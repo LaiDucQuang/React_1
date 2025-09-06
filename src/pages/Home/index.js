c6ba05d5fc3f88f024613e967cd284ff37ae494b
Lại Đức Quang <lam788145@gmail.com>
2025-09-06 10:06:03 +0700

    video

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,14 +3,7 @@ function Home() {
     <>
       <h1>Video Đánh boss rắn</h1>
       <p>Anh Quang thiện xạ đẳng cấp</p>
-      <video 
-        width="640" 
-        height="360" 
-        controls
-      >
-        <source src="/bai35/learn-react-router/src/videos/siu.mp4" type="video/mp4" />
-       
-      </video>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/zhlIg3tlkrE?si=r6Krh-_Dqa9loe_J" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </>
   );
 }
